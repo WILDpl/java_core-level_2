dddae551c435f2b6722f3b5499015d1f71976b5e
wildpl <wildpl@mail.ru>
2022-04-29 13:57:50 +0300

    Homework - 3 - Небольшие исправления в выводе информации

--- src/main/java/ru/gb/java_core/level_2/Homework_3/Homework_3.java
+++ src/main/java/ru/gb/java_core/level_2/Homework_3/Homework_3.java
@@ -6,12 +6,25 @@ public class Homework_3 {
 
     public static void main(String[] args) {
 
+        /* п.1
+        Создать массив с набором слов (10-20 слов, должны встречаться повторяющиеся).
+        Найти и вывести список уникальных слов, из которых состоит массив (дубликаты не считаем).
+        Посчитать сколько раз встречается каждое слово.
+        */
         String[] arrTest = {"Апрель", "Март", "Февраль", "Май", "Июнь", "Январь", "Февраль", "Март", "Октябрь",
                 "Апрель", "Июнь", "Январь", "Февраль", "Ноябрь", "Декабрь", "Июль", "Август", "Сентябрь", "Март", "Июнь"};
-        int initCapacity = 20;
+        int initCapacity = arrTest.length;
 
-//        analysisArrayList(arrTest, initCapacity);
+        analysisArrayList(arrTest, initCapacity);
+        System.out.println();
 
+        /* п.2
+        Написать простой класс ТелефонныйСправочник, который хранит в себе список фамилий и телефонных номеров.
+        В этот телефонный справочник с помощью метода add() можно добавлять записи.
+        С помощью метода get() искать номер телефона по фамилии.
+        Следует учесть, что под одной фамилией может быть несколько телефонов (в случае однофамильцев),
+        тогда при запросе такой фамилии должны выводиться все телефоны.
+        * */
         Phonebook book = new Phonebook();
         book.addNewRow("Иванов И.В.","7(000)00-00-001");
         book.addNewRow("Петров А.С.","7(000)00-00-002");
@@ -21,11 +34,8 @@ public class Homework_3 {
         book.addNewRow("Кузнецов В.С.","7(000)00-00-006");
         book.addNewRow("Пупкин И.И.","7(000)00-00-007");
         book.addNewRow("Уткин В.С.","7(000)00-00-008");
-        System.out.println(book.getPhoneBook());
+        book.getPhoneBook();
         book.findPhone("Иванов");
-
-
-
 
     }
 
@@ -39,7 +49,7 @@ public class Homework_3 {
 
         System.out.printf("Полная коллекция данных [%d]: %s\n", fullArray.size(), fullArray);
         System.out.printf("Коллекция уникальных данных [%d]: %s\n\n", distinctArray.size(), distinctArray);
-        System.out.println("Количество повторяющихся элементов в полной коллекции:");
+        System.out.println("Количество повторений элементов в полной коллекции:");
 
         for (String e : distinctArray) {
             int count = 0;
@@ -65,17 +75,23 @@ public class Homework_3 {
         }
 
         public void findPhone(String name) {
+            System.out.printf("Поиск: %s\n", name);
             for (Map.Entry<String, String> entry : phoneBook.entrySet()) {
                 if (entry.getKey().contains(name)) {
-                    System.out.println(entry.getValue());
+                    System.out.println(entry.getKey() + "  \t\t" + entry.getValue());
                 }
             }
         }
 
-        public HashMap<String, String> getPhoneBook() {
-            return phoneBook;
+        public void getPhoneBook() {
+            StringBuilder book = new StringBuilder();
+            book.append("Телефонный справочник:\n");
+            for (Map.Entry<String, String> entry : phoneBook.entrySet()) {
+                // можно было вывести только номера телефонов, но решил вывести полную информацию
+                book.append(entry.getKey()).append("  \t\t").append(entry.getValue()).append("\n");
+            }
+            System.out.println(book);
         }
     }
-
 
 }
